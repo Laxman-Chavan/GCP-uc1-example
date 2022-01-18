5e6956d4461ab8b27752a429051888d2017ee65b
Chavan <195570@NTTDATA.COM>
2022-01-18 20:06:40 +0530

    updated codesrc/main/java/com/gcp/lab1/example/App.java

--- src/main/java/com/gcp/lab1/example/CommonLog.java
+++ src/main/java/com/gcp/lab1/example/CommonLog.java
@@ -2,20 +2,24 @@ package com.gcp.lab1.example;
 
 import org.apache.beam.sdk.schemas.JavaFieldSchema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
+import org.apache.beam.sdk.schemas.annotations.SchemaCreate;
 
 /**
- * A class used for parsing JSON web server events
- * Annotated with @DefaultSchema to the allow the use of Beam Schema and <Row> object
+ * A class used for parsing JSON web server events Annotated with @DefaultSchema
+ * to the allow the use of Beam Schema and <Row> object
  */
 @DefaultSchema(JavaFieldSchema.class)
 public class CommonLog {
-    String user_id;
-    String ip;
-    @javax.annotation.Nullable Double lat;
-    @javax.annotation.Nullable Double lng;
-    String timestamp;
-    String http_request;
-    String user_agent;
-    Long http_response;
-    Long num_bytes;
+	int id;
+	@javax.annotation.Nullable
+	String name;
+	@javax.annotation.Nullable
+	String surname;
+
+	 @SchemaCreate
+	    public CommonLog(int id,String name,String surname) {
+	     this.id=id;
+	     this.name=name;
+	     this.surname=surname;
+	    }
 }
