6235e4c85e1e5340d70c76085560c809cd51632c
Chavan <195570@NTTDATA.COM>
2022-01-18 20:10:15 +0530

    updated code

--- src/main/java/com/gcp/lab1/example/App.java
+++ src/main/java/com/gcp/lab1/example/App.java
@@ -5,19 +5,18 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.*;
-import org.joda.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.google.gson.Gson;
 import org.apache.beam.runners.dataflow.DataflowRunner;
-import com.google.api.services.bigquery.model.TableRow;
-import com.google.api.services.bigquery.model.TableSchema;
-import com.google.api.services.bigquery.model.TableFieldSchema;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.sdk.values.TupleTagList;
+import org.apache.beam.sdk.values.Row;
+import org.apache.beam.sdk.transforms.ParDo;
 
 /**
  * Class for executing a Apache Beam pipeline
@@ -28,28 +27,38 @@ public class App {
 	 * The logger to output status messages to.
 	 */
 	private static final Logger LOG = LoggerFactory.getLogger(App.class);
-	
-	private static final String JOB_NAME="usecase1-labid-2";
-	private static final String REGION="europe-west4";
-	private static final String GCP_TEMP_LOCATION="gs://c4e-uc1-dataflow-temp-2/temp";
-	private static final String STAGING_LOCATION="gs://c4e-uc1-dataflow-temp-2/staging";
-	private static final String SERVICE_ACCOUNT="c4e-uc1-sa-2@nttdata-c4e-bde.iam.gserviceaccount.com";
-	private static final String WORKER_MACHINE_TYPE="n1-standard-1";
-	private static final String SUBNETWORK="regions/europe-west4/subnetworks/subnet-uc1-2";
-	
-	private static final String BQ_TABLE="nttdata-c4e-bde:uc1_2.account";
-	private static final String TOPIC_SUB="projects/nttdata-c4e-bde/subscriptions/uc1-input-topic-sub-2";
+	// declared input paramters
+	private static final String JOB_NAME = "usecase1-labid-2";
+	private static final String REGION = "europe-west4";
+	private static final String GCP_TEMP_LOCATION = "gs://c4e-uc1-dataflow-temp-2/temp";
+	private static final String STAGING_LOCATION = "gs://c4e-uc1-dataflow-temp-2/staging";
+	private static final String SERVICE_ACCOUNT = "c4e-uc1-sa-2@nttdata-c4e-bde.iam.gserviceaccount.com";
+	private static final String WORKER_MACHINE_TYPE = "n1-standard-1";
+	private static final String SUBNETWORK = "regions/europe-west4/subnetworks/subnet-uc1-2";
+	// declared bq table and pub-sub topic
+	private static final String BQ_TABLE = "nttdata-c4e-bde:uc1_2.account";
+	private static final String TOPIC_SUB = "projects/nttdata-c4e-bde/subscriptions/uc1-input-topic-sub-2";
+	private static final String DLQ_TOPIC = "projects/nttdata-c4e-bde/topics/uc1-dlq-topic-2";
 
-	
-  
+	public static final TupleTag<CommonLog> VALID_MSG = new TupleTag<CommonLog>() {
+		private static final long serialVersionUID = 1L;
+	};
+	public static final TupleTag<String> INVALID_MSG = new TupleTag<String>() {
+		private static final long serialVersionUID = 1L;
+	};
+
+	public static final Schema BQ_SCHEMA = Schema.builder().addInt64Field("id").addStringField("name")
+			.addStringField("surname").build();
+
 	/**
 	 * The main entry-point for pipeline execution. This method will start the
 	 * pipeline but will not wait for it's execution to finish. If blocking
 	 * execution is required, use the
 	 * 
 	 * @param args The command-line args passed by the executor.
+	 * @throws Exception
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		DataflowPipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
 				.as(DataflowPipelineOptions.class);
 		// set the command line attributes
@@ -63,49 +72,66 @@ public class App {
 		options.setSubnetwork(SUBNETWORK);
 		options.setStreaming(true);
 
-		runPipeline(options);
+		run(options);
 
+	}
+
+	public static class JsonToCommonLog extends DoFn<String, CommonLog> {
+
+		private static final long serialVersionUID = 1L;
+
+		public static PCollectionTuple convert(PCollection<String> input) throws Exception {
+			return input.apply("JsonToCommonLog", ParDo.of(new DoFn<String, CommonLog>() {
+
+				private static final long serialVersionUID = 1L;
+
+				@ProcessElement
+				public void processElement(@Element String record, ProcessContext context) {
+
+					try {
+						Gson gson = new Gson();
+						CommonLog commonLog = gson.fromJson(record, CommonLog.class);
+						context.output(VALID_MSG, commonLog);
+					} catch (Exception e) {
+						e.printStackTrace();
+						context.output(INVALID_MSG, record);
+					}
+				}
+			}).withOutputTags(VALID_MSG, TupleTagList.of(INVALID_MSG)));
+		}
 	}
 
 	// method to execute the apache beam pipeline
-	public static void runPipeline(DataflowPipelineOptions options) {
-		// Build the table schema for the output table.
-		List<TableFieldSchema> fields = new ArrayList<>();
-		fields.add(new TableFieldSchema().setName("name").setType("STRING"));
-		fields.add(new TableFieldSchema().setName("surname").setType("STRING"));
-		fields.add(new TableFieldSchema().setName("id").setType("INTEGER"));
-		TableSchema schema = new TableSchema().setFields(fields);
-		// created pipeline object
+	public static void run(DataflowPipelineOptions options) throws Exception {
 		Pipeline p = Pipeline.create(options);
-		p.apply("ReadPubSubMessage",
-				PubsubIO.readMessagesWithAttributes()
-						.fromSubscription(TOPIC_SUB))
-				.apply("WindowByMinute", Window.into(FixedWindows.of(Duration.standardSeconds(60))))
 
-				.apply("ConvertDataToTableRows", ParDo.of(new DoFn<PubsubMessage, TableRow>() {
-					@ProcessElement
-					public void processElement(ProcessContext c) {
-						PubsubMessage message = c.element();
-						int id = 0;
-						try {
-							id = Integer.parseInt(message.getAttribute("id"));
-						} catch (NumberFormatException e) {
-							e.getMessage();
-						}
-						// get the attributes from pub sub message
-						String name = message.getAttribute("name");
-						String surname = message.getAttribute("surname");
-						// set the attributes in the table row
-						TableRow row = new TableRow().set("id", id).set("name", name).set("surname", surname);
-						c.output(row);
-					}
-				})).apply("InsertTableRowsToBigQuery",
-						BigQueryIO.writeTableRows().to(BQ_TABLE).withSchema(schema)
-								.withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+		PCollection<String> pubSubMessages = p.apply("ReadPubSubMessage",
+				PubsubIO.readStrings().fromSubscription(TOPIC_SUB));
+
+		PCollectionTuple pubSubMessagesdata = JsonToCommonLog.convert(pubSubMessages);
+
+		pubSubMessagesdata.get(VALID_MSG).apply("ConvertCommonLogToJson", ParDo.of(new DoFn<CommonLog, String>() {
+			private static final long serialVersionUID = 1L;
+
+			@ProcessElement
+			public void processElement(ProcessContext context) {
+				Gson gsonObj = new Gson();
+				String jsonRecord = gsonObj.toJson(context.element());
+				context.output(jsonRecord);
+			}
+		}))
+
+				.apply("ConvertJsontoRow", JsonToRow.withSchema(BQ_SCHEMA))
+
+				.apply("InserttoBQTable",
+						BigQueryIO.<Row>write().to(BQ_TABLE).useBeamSchema()
+								.withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_NEVER)
 								.withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
-
+		/**
+		 * Writing INVALID Messages into the DEAD LETTER TOPIC
+		 */
+		pubSubMessagesdata.get(INVALID_MSG).apply("WriteToDLQTopic", PubsubIO.writeStrings().to(DLQ_TOPIC));
 		LOG.info("Building pipeline......................");
 		p.run().waitUntilFinish();
 	}
-
 }
